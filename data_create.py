4a204c9efc0a9f4ec773c14cd77bc86e3bda8cb1
Margarita <kovelkova.m@gmail.com>
2024-07-31 00:46:00 +0300

    создание файла для ввода данных

---
+++ data_create.py
@@ -0,0 +1,20 @@
+def name_data():
+    name = input('введите имя  ')
+    print("очень красивое имя")
+    return name
+
+def surname_data():
+    surname = input('введите фамилию  ')
+    print("фамилия класс")
+    return surname
+
+def phone_data():
+    phone = input('введите тел  ')
+    print("очень красивое tel")
+    return phone
+
+def address_data():
+    address = input('введите адрес  ')
+    print("очень красивое address")
+    return address
+
