2b5f29fbc1c98c747db29de8aee832301ea63521
Margarita <kovelkova.m@gmail.com>
2024-07-31 11:15:33 +0300

    запуск всех команд

--- ui.py
+++ ui.py
@@ -1,19 +1,23 @@
-from logger import input_data, print_data
+from logger import input_data, print_data, rename_data, delet_data
 
 
 
 def interface():
-    print("Добрый день! Я бот. \n 1 - запись данных \n 2 - вывод данных")
+    print("Добрый день! Я бот. \n 1 - запись данных \n 2 - вывод данных \n 3 - изменить данные \n 4 - удалить данные")
     command = int(input("введите число "))
 
-    while command != 1 and command != 2:
+    while command != 1 and command != 2 and command != 3 and command != 4:
         print("Ошибка ввода")
         command = int(input("введите число "))
 
     if command == 1:
         input_data()
-    elif  command == 2:
+    if  command == 2:
         print_data()
+    if command == 3:
+        rename_data()
+    if command == 4:
+        delet_data()
 
 
 
